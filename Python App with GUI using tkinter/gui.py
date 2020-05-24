6cd6d08cbcea4677861c2c27b727a52d008aafce
Prathamesh <prathmesh613@gmail.com>
2020-05-24 16:36:03 +0530

    GUI using python

--- Python App with GUI using tkinter/gui.py
+++ Python App with GUI using tkinter/gui.py
@@ -2,13 +2,10 @@ from tkinter import *
 
 root = Tk()
 
-myLabel1 = Label(root, text = "Hello, World!")
-myLabel2 = Label(root, text = "My Name is Prathamesh")
-myLabel3 = Label(root, text = "How are you?")
-# Shove it
-myLabel1.grid(row = 0, column = 0)
-myLabel2.grid(row = 2, column = 2)
-myLabel3.grid(row = 1, column = 1)
-#Event Loop is continually Loop
-#program ends, loop ends
+def myClick():
+    myLabel = Label(root, text = "Look!, I clicked a Button")
+
+myButton = Button(root,text = "Click Me!", padx = 50, pady = 75, command = myClick)
+myButton.pack()
+
 root.mainloop()
